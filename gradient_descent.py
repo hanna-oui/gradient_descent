739245517d93bf1684dcc67b2ad7df245eacec0a
hanna-oui <ozhannaoui@gmail.com>
2024-12-05 21:38:29 +0100

    Implemented gradient descent and Armijo-Goldstein algorithm.

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,11 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os 
-import math
-
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 class Plot:
     def __init__(self, func):
@@ -35,61 +29,98 @@ class Plot:
         plt.show()
 
 class Rosenbrock:
-
-    name = "Rosenbrock's function"
+    name = "Rosenbrock"
     equation = r"$f(x, y) = (1 - x)^2 + 100(y - x^2)^2$"
 
     @staticmethod
-    def function(x, y):
+    def fn(x, y):
         return (1 - x)**2 + 100 * (y - x**2)**2
     
     @staticmethod
-    def log_function(x, y):
+    def log_fn(x, y):
         return np.log((1 - x)**2 + 100 * (y - x**2)**2)
+    
+    @staticmethod 
+    def function(X):
+        return (1 - X[0])**2 + 100 * (X[1] - X[0]**2)**2
 
     @staticmethod
-    def gradient(x, y):
-        dx = -2 * (1 - x) - 400 * x * (y - x**2)
-        dy = 200 * (y - x**2)
+    def gradient(X):
+        dx = -2 * (1 - X[0]) - 400 * X[0] * (X[1] - X[0]**2)
+        dy = 200 * (X[1] - X[0]**2)
         return np.array([dx, dy])
 
     @staticmethod
-    def hessian(x, y):
-        dxx = 2 - 400 * (y - 3 * x**2)
-        dxy = -400 * x
+    def hessian(X):
+        dxx = 2 - 400 * (X[1] - 3 * X[1]**2)
+        dxy = -400 * X[0]
         dyy = 200
         return np.array([[dxx, dxy], [dxy, dyy]])
 
 
 class Himmelblau:
-
-    name = "Himmelblau's function"
+    name = "Himmelblau"
     equation = r"$f(x,y)=(x^2+y-11)^2 + (x + y^2 - 7)^2$"
 
     @staticmethod
-    def function(x, y):
+    def fn(x, y):
         return (x**2 + y - 11)**2 + (x + y**2 - 7)**2
     
     @staticmethod
-    def log_function(x,y):
+    def log_fn(x,y):
         return np.log((x**2 + y - 11)**2 + (x + y**2 - 7)**2)
 
     @staticmethod
-    def gradient(x, y):
-        dx = 4 * x * (x**2 + y - 11) + 2 * (x + y**2 - 7)
-        dy = 2 * (x**2 + y - 11) + 4 * y * (x + y**2 - 7)
+    def function(X):
+        return (X[0]**2 + X[1] - 11)**2 + (X[0] + X[1]**2 - 7)**2
+
+    @staticmethod
+    def gradient(X):
+        dx = 4 * X[0] * (X[0]**2 + X[1] - 11) + 2 * (X[0] + X[1]**2 - 7)
+        dy = 2 * (X[0]**2 + X[1] - 11) + 4 * X[1] * (X[0] + X[1]**2 - 7)
         return np.array([dx, dy])
 
     @staticmethod
-    def hessian(x, y):
-        dxx = 12 * x**2 + 4 * y - 42
-        dxy = 4 * x + 4 * y
-        dyy = 12 * y**2 + 4 * x - 26
+    def hessian(X):
+        dxx = 12 * X[0]**2 + 4 * X[1] - 42
+        dxy = 4 * X[0] + 4 * X[1]
+        dyy = 12 * X[1]**2 + 4 * X[0] - 26
         return np.array([[dxx, dxy], [dxy, dyy]])
+    
 
+class GradientDescent:
+    function_mapping = {
+        "Rosenbrock" : Rosenbrock,  
+        "Himmelblau" : Himmelblau
+    }
 
-# example usage
-Plot(Rosenbrock.function).density_3d(x_range=[-2,2],y_range=[-1,3])
-plt.close()
+    def __init__(self, function_name):
+        function_class = self.function_mapping[function_name]
+        self.f = function_class.function
+        self.grad = function_class.gradient 
+    
+    def armijo_step(self, xk, beta=0.5, delta=0.1):
+        tau = 1.0  # Initial step size
+        gradient = self.grad(xk)
+        while self.f(xk - tau * gradient) > self.f(xk) - delta * tau * np.dot(gradient, gradient):
+            tau *= beta
+        return tau
+    
+    def optimize(self, x0, step_type, fixed_tau=0.001, epsilon=10e-5):
+        xk = x0 
+        i = 0
+        while np.linalg.norm(self.grad(xk)) > epsilon:
+            if step_type == "armijo":
+                tau = self.armijo_step(xk)
+            if step_type == "fixed":
+                tau = fixed_tau
+            xk = xk - tau*self.grad(xk) 
+            i+=1
+            print(f"Iteration: {i}")
+            print(f"Gradient Norm: {np.linalg.norm(self.grad(xk))}")
+            print(f"Function Value: {self.f(xk)}")
+            print(f"Current point {xk}")
+        return xk, i
+            
 
-
+print(GradientDescent("Himmelblau").optimize([np.pi+1,np.pi-1], "fixed"))
